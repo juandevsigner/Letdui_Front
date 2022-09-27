5029f14be1e384f9e48d4a4a88b7f68a5552ed55
Juan Da <juanda.core07@gmail.com>
2022-09-27 12:34:33 -0500

    bg

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export const AuthLayout = () => {
   return (
-    <div className="h-screen flex justify-center items-center bg-[url('./src/assets/background.svg')] bg-cover bg-center">
+    <div className="h-screen flex justify-center items-center bg-[url('../assets/background.svg')] bg-cover bg-center">
       <main className="container mx-auto md:mt-20 p-5 md:flex md:justify-center">
         <div className="md:w-2/3 lg:w-2/5">
           <Outlet />
